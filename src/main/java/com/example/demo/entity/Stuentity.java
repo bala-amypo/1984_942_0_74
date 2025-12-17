5a43fe940a92da104a7908fe4a5dca8b23e21681
bala-amypo <bala@amypo.in>
2025-12-17 05:02:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stuentity.java
+++
@@ -1,3 +0,0 @@
-hello guys i am akash from cyber security
-who loves to fk everyone in the the class including every gender in this world and also every thing in this world can be fked by me!!
-long time ago a boy was born in tirunelveli,tamil nadu,no one knows about his urge to fk everything like cats ,rats and even objects ,so he                      
